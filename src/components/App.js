90866a9de6bfd0b1080e5208b638e4f8cecb4b2d
Kenny Ek <ekkenny@hotmail.com>
2018-07-08 16:19:00 +0200

    Refactor to use Redux

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import user from "../images/user.png";
+import "./App.css";
+
+const App = props => {
+  const { name, description, likes, location } = props;
+
+  return (
+    <div className="App">
+      <section className="User__img">
+        <img src={user} alt="user" />
+      </section>
+
+      <section className="User__info">
+        <p>
+          {" "}
+          <span className="faint">I am</span> a {description}
+        </p>
+        <p>
+          {" "}
+          <span className="faint">I like</span> {likes}
+        </p>
+
+        <p className="User__info__details User__info__divider faint">
+          <span>Name: </span>
+          <span>{name}</span>
+        </p>
+        <p className="User__info__details faint">
+          <span>Location: </span>
+          <span>{location}</span>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  likes: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired
+}
+
+export default App;
